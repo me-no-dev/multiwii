dbea57a9de19510096bfc8cbac5b7ea81798f4f6
danal.estes <danal.estes@gmail.com>
2011-09-01 20:00:59 +0000

    Alpha code for Spektrum Satellite support.  Bench tested; not yet flight tested.

--- MultiWii1_8+Spektrum/def.h
+++ MultiWii1_8+Spektrum/def.h
@@ -176,6 +176,9 @@
   #define DIGITAL_BI_LEFT_HIGH       PORTB |= 1<<3;
   #define DIGITAL_BI_LEFT_LOW        PORTB &= ~(1<<3);
   #define PPM_PIN_INTERRUPT          attachInterrupt(0, rxInt, RISING); //PIN 0
+  #define SPEK_SERIAL_VECT           USART_RX_vect
+  #define SPEK_SERIAL_INTERRUPT      UCSR0B |= (1<<RXEN0)|(1<<RXCIE0);     
+  #define SPEK_DATA_REG              UDR0
   #define MOTOR_ORDER                9,10,11,3,6,5  //for a quad+: rear,right,left,front
   #define DIGITAL_CAM_PINMODE        pinMode(A2,OUTPUT);
   #define DIGITAL_CAM_HIGH           PORTC |= 1<<2;
@@ -225,6 +228,9 @@
   #define DIGITAL_BI_LEFT_HIGH       PORTH |= 1<<3;
   #define DIGITAL_BI_LEFT_LOW        PORTH &= ~(1<<3);
   #define PPM_PIN_INTERRUPT          attachInterrupt(4, rxInt, RISING);  //PIN 19, also used for Spektrum satellite option
+  #define SPEK_SERIAL_VECT           USART1_RX_vect
+  #define SPEK_SERIAL_INTERRUPT      UCSR1B |= (1<<RXEN1)|(1<<RXCIE1);  
+  #define SPEK_DATA_REG              UDR1
   #define MOTOR_ORDER                3,5,6,2,7,8,9,10   //for a quad+: rear,right,left,front   //+ for y6: 7:under right  8:under left
   #define DIGITAL_CAM_PINMODE        pinMode(33,OUTPUT); pinMode(46,OUTPUT); // 33 + 46
   #define DIGITAL_CAM_HIGH           PORTC |= 1<<4;PORTL |= 1<<3;
@@ -281,3 +287,5 @@
 #elif defined(OCTOFLATX)
   #define MULTITYPE 11      //the GUI is the same for all 8 motor configs
 #endif
+
+
