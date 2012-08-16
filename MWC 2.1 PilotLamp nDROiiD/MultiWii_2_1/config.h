f8efc7c6d575f1ec576f7b6bab4425edd1c64c64
nils.tholen <nils.tholen@02679b44-d973-9852-f2fa-63770883b36c>
2012-08-16 21:09:38 +0000

    final beta

--- MWC 2.1 PilotLamp nDROiiD/MultiWii_2_1/config.h
+++ MWC 2.1 PilotLamp nDROiiD/MultiWii_2_1/config.h
@@ -704,10 +704,10 @@
   /********************************************************************/
   /****                 Buzzer & Pilot Lampt                       ****/
   /********************************************************************/
-    //#define BUZZER                //uncomment if a buzzer is connected to the FC
+    #define BUZZER                //uncomment if a buzzer is connected to the FC
     #define RCOPTIONSBEEP        //uncomment this if you want the buzzer to beep at any rcOptions change on channel Aux1 to Aux4
     #define ARMEDTIMEWARNING 330  // Trigger an alarm after a certain time of being armed [s]
-    #define PILOTLAMP            //Uncomment if you are using a X-Arcraft Pilot Lamp
+    //#define PILOTLAMP            //Uncomment if you are using a X-Aircraft Pilot Lamp
 
   /********************************************************************/
   /****           battery voltage monitoring                       ****/
