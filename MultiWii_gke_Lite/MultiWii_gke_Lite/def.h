f2c458021045e61ef0b5078dfaed442e4796e06a
gke <gke@optusnet.com.au>
2013-09-29 10:00:32 +0000

    Update 20130929.

--- MultiWii_gke_Lite/MultiWii_gke_Lite/def.h
+++ MultiWii_gke_Lite/MultiWii_gke_Lite/def.h
@@ -1,20 +1,42 @@
+/*
 
-// LIMIT1 MACRO TO REPLACE SYMMETRIC CONSTRAINS
+ MWLite_gke
+ May 2013
+ 
+ MWLite_gke is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version. see <http://www.gnu.org/licenses/>
+ 
+ MWLite_gke is distributed in the hope that it will be useful,but WITHOUT ANY 
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+ A PARTICULAR PURPOSE. 
+ 
+ See the GNU General Public License for more details.
+ 
+ Lite was based originally on MultiWiiCopter V2.2 by Alexandre Dubus
+ www.multiwii.com, March  2013. The rewrite by Prof Greg Egan was renamed 
+ so as not to confuse it with the original.
+ 
+ It preserves the parameter specification and GUI interface with parameters
+ scaled to familiar values. 
+ 
+ Major changes include the control core which comes from UAVX with the 
+ addition of MW modes.
+ 
+ Lite supports only Atmel 32u4 processors using an MPU6050 and optionally 
+ BMP085 and MS5611 barometers and HMC5883 magnetometer with 4 motors, 
+ no servos and 8KHz PWM for brushed DC motors.
+ 
+ */
+ 
+// Macros
 
 #define Limit1(i,l) (((i) < -(l)) ? -(l) : (((i) > (l)) ? (l) : (i)))
+#define Sign(n) ((n<0) ? -1 : 1)
+#define PercentToStick(n) (n*5)
 
-/**************************************************************************************/
-/***************             Proc specific definitions             ********************/
-/**************************************************************************************/
-// Proc auto detection
-
-#if defined(__AVR_ATmega32U4__) 
-#define PROMICRO
-#endif
-
-/**************************************************************************************/
-/***************             motor and servo numbers               ********************/
-/**************************************************************************************/
+// Motors
 
 #define NUMBER_MOTOR     4
 
@@ -24,26 +46,28 @@
 #define EFF_MINCOMMAND conf.minthrottle
 #endif
 
-/**************************  atmega32u4 (Promicro)  ***********************************/
-
-#define LEDPIN_PINMODE             //
-#define LEDPIN_TOGGLE              PIND |= 1<<5     //switch LEDPIN state (Port D5)
-#if !defined(PROMICRO10)
-#define LEDPIN_OFF                 PORTD |= (1<<5)
-#define LEDPIN_ON                  PORTD &= ~(1<<5)
-#else
-#define LEDPIN_OFF                PORTD &= ~(1<<5)
-#define LEDPIN_ON                 PORTD |= (1<<5)
+#if (defined(USE_MW_CONTROL) || defined(USE_MW_BASEFLIGHT_CONTROL) || defined(USE_MW_EXPANDED_CONTROL))
+#define USE_MW
 #endif
 
-#define POWERPIN_PINMODE           //
-#define POWERPIN_ON                //
-#define POWERPIN_OFF               //
+#if defined(USE_QUICK_TUNE) || defined(USE_QUICK_ALT_TUNE) || defined(USE_RELAY_TUNE)
+#define USE_TUNING
+#endif
+
+#if defined(USE_BOSCH_MAG)
+#define USE_MAG
+#endif
+
+/// atmega32u4 
+
+#define LED_BLUE_PINMODE             //
+#define LED_BLUE_PIN                PORTD5
+#define LED_BLUE_TOGGLE             PIND |= 1<<5     //switch LEDPIN state (Port D5)
+#define LED_BLUE_OFF                PORTD |= (1<<5)
+#define LED_BLUE_ON                 PORTD &= ~(1<<5)
+
 #define I2C_PULLUPS_ENABLE         PORTD |= 1<<0; PORTD |= 1<<1   // PIN 2&3 (SDA&SCL)
 #define I2C_PULLUPS_DISABLE        PORTD &= ~(1<<0); PORTD &= ~(1<<1)
-#define STABLEPIN_PINMODE          ;
-#define STABLEPIN_ON               ;
-#define STABLEPIN_OFF              ;
 #define PPM_PIN_INTERRUPT          DDRE &= ~(1 << 6);PORTE |= (1 << 6);EIMSK |= (1 << INT6);EICRB |= (1 << ISC61)|(1 << ISC60)
 #if !defined(SPEK_SERIAL_PORT)
 #define SPEK_SERIAL_PORT           1
@@ -51,8 +75,8 @@
 #define USB_CDC_TX                 3
 #define USB_CDC_RX                 2
 
-//Standard RX
-#define THROTTLEPIN                  3
+// Standard RX
+#define THROTTLEPIN                3
 #if defined(A32U4ALLPINS)
 #define ROLLPIN                    6
 #define PITCHPIN                   2
@@ -67,6 +91,7 @@
 #define AUX2PIN                      7 
 #define AUX3PIN                      1 // unused 
 #define AUX4PIN                      0 // unused 
+
 #if !defined(RCAUX2PIND17)
 #define PCINT_PIN_COUNT          4
 #define PCINT_RX_BITS            (1<<1),(1<<2),(1<<3),(1<<4)
@@ -80,21 +105,7 @@
 #define RX_PC_INTERRUPT              PCINT0_vect
 #define RX_PCINT_PIN_PORT            PINB
 
-#if !defined(A32U4ALLPINS) 
-#define V_BATPIN                  A3    // Analog PIN 3
-#elif defined(A32U4ALLPINS)
-#define V_BATPIN                  A4    // Analog PIN 4
-#else
-#define V_BATPIN                  A2    // Analog PIN 3
-#endif
-
-#define PSENSORPIN                A2    // Analog PIN 2 
-
-
-/**************************************************************************************/
-/***************      IMU Orientations and Sensor definitions      ********************/
-/**************************************************************************************/
-
+// Board Orientations and Sensor definitions
 
 #if defined(NANOWII)
 #define QUADX
@@ -103,24 +114,37 @@
 #define GYRO_ORIENTATION(X, Y, Z) {gyroADC[ROLL] = -X; gyroADC[PITCH] = -Y; gyroADC[YAW] = -Z;}
 #undef INTERNAL_I2C_PULLUPS
 
-#define LEDPIN_PINMODE             DDRD |= (1<<4)           //D4 to output
-#define LEDPIN_TOGGLE              PIND |= (1<<5)|(1<<4)     //switch LEDPIN state (Port D5) & pin D4
-#define LEDPIN_OFF                 PORTD |= (1<<5); PORTD &= ~(1<<4)
-#define LEDPIN_ON                  PORTD &= ~(1<<5); PORTD |= (1<<4)  
+#define LED_BLUE_PINMODE             DDRD |= (1<<4)           //D4 to output
+#define LED_BLUE_TOGGLE              PIND |= (1<<5)|(1<<4)     //switch LEDPIN state (Port D5) & pin D4
+#define LED_BLUE_OFF                 PORTD |= (1<<5); PORTD &= ~(1<<4)
+#define LED_BLUE_ON                  PORTD &= ~(1<<5); PORTD |= (1<<4)
+
+#define VBAT_PIN A3
+#define VOLTS_RTOP 58  // 56 Ratios as per HK NanoWii manual for 3S pack
+#define VOLTS_RBOT 33  
+
+#define SONAR_PIN A1 // ??????
+
 #endif
 
 #if defined(HK_PocketQuad)
 #define QUADX
 #define DC_MOTORS
+#define CESCO_OFFSET // motor PWM phase offset to smooth battery load
 #define MPU6050
 #define ACC_ORIENTATION(X, Y, Z)  {accADC[ROLL]  = -X; accADC[PITCH]  =  -Y; accADC[YAW]  =  Z;}
 #define GYRO_ORIENTATION(X, Y, Z) {gyroADC[ROLL] = Y; gyroADC[PITCH] = -X; gyroADC[YAW] = -Z;}
 #undef INTERNAL_I2C_PULLUPS
+
+#define VBAT_PIN A8
+#define VOLTS_RTOP 0  // no divider for 1S pack
+#define VOLTS_RBOT 1
+
+#define SONAR_PIN A1 // ??????
+
 #endif
 
-/**************************************************************************************/
-/***************      Multitype decleration for the GUI's          ********************/
-/**************************************************************************************/
+//Multitype declaration for the GUI's 
 
 #if defined(QUADP)
 #define MULTITYPE 2
@@ -128,14 +152,9 @@
 #define MULTITYPE 3
 #endif
 
-/**************************************************************************************/
-/***************          Some unsorted "chain" defines            ********************/
-/**************************************************************************************/
+// Some unsorted "chain" defines 
 
-//all new Special RX's must be added here
-//this is to avoid confusion :)
-
-#if !defined(SERIAL_SUM_PPM) && !defined(SPEKTRUM) && !defined(SBUS)
+#if !defined(SERIAL_SUM_PPM) && !defined(SPEKTRUM) && !defined(SBUS) && !defined(FLYSKY)
 #define STANDARD_RX
 #endif
 
@@ -158,7 +177,9 @@
 #if defined(SBUS)
 #define RC_CHANS 18
 #elif defined(SPEKTRUM) 
-#define RC_CHANS 12
+#define RC_CHANS 14
+#elif defined(FLYSKY)
+#define RC_CHANS 8
 #elif defined(SERIAL_SUM_PPM)
 #define RC_CHANS 12
 #else
@@ -169,30 +190,207 @@
 #define ACROTRAINER_MODE 1000
 #endif
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// End register definition 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-/**************************************************************************************/
-/********* enforce your sensors orientation - possibly overriding board defaults  *****/
-/**************************************************************************************/
-#ifdef FORCE_GYRO_ORIENTATION
-#define GYRO_ORIENTATION FORCE_GYRO_ORIENTATION
-#endif
-#ifdef FORCE_ACC_ORIENTATION
-#define ACC_ORIENTATION FORCE_ACC_ORIENTATION
+#if !defined(ACC_TRIM_STEP_SIZE)
+#define ACC_TRIM_STEP_SIZE 8
 #endif
 
-/**************************************************************************************/
-/***************               Error Checking Section              ********************/
-/**************************************************************************************/
+#if !defined(ALT_HOLD_THROTTLE_NEUTRAL_ZONE)
+#define ALT_HOLD_THROTTLE_NEUTRAL_ZONE 40
+#endif
 
-#ifndef NUMBER_MOTOR
+#if defined(USE_MW)
+#define  VERSION  1000 
+#else
+#define  VERSION  1001 
+#endif
+
+/*********** RC alias *****************/
+enum rc { // must be in this order
+  ROLL,
+  PITCH,
+  YAW,
+  THROTTLE,
+  AUX1,
+  AUX2,
+  AUX3,
+  AUX4
+};
+
+enum pid {
+  PIDROLL,
+  PIDPITCH,
+  PIDYAW,
+  PIDALT,
+  PIDPOS,
+  PIDPOSR,
+  PIDNAVR,
+  PIDLEVEL,
+  PIDMAG,
+  PIDVEL, 
+  PIDITEMS
+};
+
+enum box {
+  BOX_ARM,
+  BOX_ANGLE,
+  BOX_HORIZON,
+  BOX_ACRO_TRAINER,
+  BOX_ALT_HOLD,
+  BOX_HEAD_FREE,
+  BOX_HEAD_HOLD,
+#if defined(USE_TUNING)
+  BOX_TUNE,
+  BOX_ALT_TUNE,
+ // BOX_RELAY,
+#endif
+  CHECKBOX_ITEMS
+};
+
+
+struct flags_struct {
+uint8_t OK_TO_ARM :
+  1 ;
+uint8_t ARMED :
+  1 ;
+uint8_t ACC_CALIBRATED :
+  1 ;
+uint8_t ANGLE_MODE :
+  1 ;
+uint8_t HORIZON_MODE :
+  1 ;
+uint8_t ALT_HOLD_MODE :
+  1 ;  
+uint8_t ACRO_TRAINER_MODE :
+  1 ; 
+uint8_t HEAD_FREE_MODE :
+  1 ;
+uint8_t HEAD_HOLD_MODE :
+  1 ;
+uint8_t TUNE_MODE :
+  1;
+uint8_t ALT_TUNE_MODE :
+  1;
+//uint8_t RELAY_MODE : 1;
+uint8_t BARO_ACTIVE :
+  1 ; 
+uint8_t SMALL_ANGLE_25DEG :
+  1;
+uint8_t CALIBRATE_MAG :
+  1 ;
+uint8_t MAG_ACTIVE:
+  1;
+uint8_t MAG_CALIBRATED:
+  1;
+uint8_t ENABLE_ACRO_TRAINER:
+  1;
+uint8_t GPS_ACTIVE:
+  1;
+uint8_t SONAR_ACTIVE:
+  1;
+uint8_t OPTIC_ACTIVE:
+  1;
+uint8_t ALARM:
+  1;
+} 
+f;
+
+// RC
+
+#define MINCHECK 1100
+#define MAXCHECK 1900
+
+#define ROL_LO  (1<<(2*ROLL))
+#define ROL_CE  (3<<(2*ROLL))
+#define ROL_HI  (2<<(2*ROLL))
+#define PIT_LO  (1<<(2*PITCH))
+#define PIT_CE  (3<<(2*PITCH))
+#define PIT_HI  (2<<(2*PITCH))
+#define YAW_LO  (1<<(2*YAW))
+#define YAW_CE  (3<<(2*YAW))
+#define YAW_HI  (2<<(2*YAW))
+#define THR_LO  (1<<(2*THROTTLE))
+#define THR_CE  (3<<(2*THROTTLE))
+#define THR_HI  (2<<(2*THROTTLE))
+
+
+const char boxnames[] PROGMEM = // names for dynamic generation of config GUI
+"ARM;"
+"ANGLE;"
+"HORIZ;"
+"ACRO.T;"
+"A.HOLD;"
+"H.FREE;"
+"H.HOLD;"
+#if defined(USE_TUNING)
+"TUNE;"
+"A.TUNE;"
+//"RELAY;"
+#endif
+;
+
+const char pidnames[] PROGMEM =
+"ROLL;"
+"PITCH;"
+"YAW;"
+"ALT;"
+"Pos;"
+"PosR;"
+"NavR;"
+"LEVEL;"
+"MAG;"
+"VEL;"
+;
+
+const uint8_t boxids[] PROGMEM = {
+  1 << BOX_ARM, 
+  1 << BOX_ANGLE, 
+  1 << BOX_HORIZON,
+  1 << BOX_ACRO_TRAINER,
+  1 << BOX_ALT_HOLD,
+  1 << BOX_HEAD_FREE, 
+  1 << BOX_HEAD_HOLD, 
+#if defined(USE_TUNING)
+  1 << BOX_TUNE,
+  1 << BOX_ALT_TUNE,
+//  1 << BOX_RELAY,
+#endif
+};
+
+
+// Errors
+
+#if !defined(LOW_VOLTAGE_WARNING)
+#define LOW_VOLTAGE_WARNING_PERCENT 80 // scales down desired throttle "suddenly" to this percentage when low volts reached
+#endif
+
+#if !defined(LOW_VOLTAGE_TIMEOUT_S)
+#define LOW_VOLTAGE_TIMEOUT_S 3
+#endif
+
+#if !defined(I2CMPU) | !defined(I2CBARO)
+#define I2CMPU 0
+#define I2CBARO 0
+#endif
+
+#if !defined(MAG_ORIENTATION)
+#define MAG_ORIENTATION(X, Y, Z)  {magADC[ROLL]  =  X; magADC[PITCH]  =  Y; magADC[YAW]  = Z;} // GY86 upside down SW I2C
+#endif
+
+#if ((defined(USE_BOSCH_BARO) + defined(USE_MS_BARO) + defined(USE_SONAR)) > 1)
+#error "Select only ONE of SONAR or MS5611 or BMP085 barometer"
+#endif
+
+#if !defined(NUMBER_MOTOR)
 #error "NUMBER_MOTOR is not set, most likely you have not defined any type of multicopter"
 #endif
 
-#if (!defined(PROMICRO) || ((NUMBER_MOTOR !=4) || defined(SERVO)))
+#if (!defined(__AVR_ATmega32U4__) || ((NUMBER_MOTOR !=4) || defined(SERVO)))
 #error "Implementation restriction: must be exactly 4 DC_MOTORS and no SERVOS and use Atmel 32u4 (Leonard)"
 #endif
+
+#if ((defined(USE_QUICK_TUNE) + defined(USE_QUICK_ALT_TUNE) +  defined(USE_RELAY_TUNE))>1)
+#error "Only one tuning scheme permitted at a time (USE_QUICK_TUNE or USE_QUICK_ALT_TUNE)" // or USE_RELAY_TUNE)"
+#endif
+
 
 
