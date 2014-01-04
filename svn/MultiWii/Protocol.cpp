d55581d01a61692b2e729b860b83a0eb40055a07
trey.marc <trey.marc@gmail.com>
2014-01-04 11:35:24 +0000

    define MSP_PROTOCOL_EXTENSION in config.h

--- svn/MultiWii/Protocol.cpp
+++ svn/MultiWii/Protocol.cpp
@@ -15,7 +15,9 @@
 // Multiwii Serial Protocol 0 
 #define MSP_VERSION                0
 
-#define MSP_CUSTOM                 1  // unknow message    custom crafted message
+#ifdef MSP_PROTOCOL_EXTENSION
+    #define MSP_CUSTOM                 1  // unknow message    custom crafted message
+#endif
 
 //to multiwii developpers/committers : do not add new MSP messages without a proper argumentation/agreement on the forum
 #define MSP_IDENT                100   //out message         multitype + multiwii version + protocol version + capability variable
@@ -245,9 +247,11 @@ void evaluateCommand() {
   uint32_t tmp=0; 
 
   switch(cmdMSP[CURRENTPORT]) {
+  #ifdef MSP_PROTOCOL_EXTENSION
   case MSP_CUSTOM:
       headSerialError(0); // we dot have any custom msp , tell the gui we do not use that
       break;
+  #endif
    case MSP_SET_RAW_RC:
      s_struct_w((uint8_t*)&rcSerial,16);
      rcSerialCount = 50; // 1s transition 
